0d1b07c3c4ced050cc414023b31971f52fea0992
acevallos <andrescevallos@MacBook-Pro-de-Andres.local>
2014-07-22 10:54:22 -0500

    arreglo cagada hecha por mi

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 	$fecha = date("d-M-y H:i");
-	$sujeto = "Mensaje desde la web";
+	$sujeto = "Mensaje desde el sitio web consultoriografico.com";
 	$contenido = "Nombre: ".$_POST['nombre']."\r\n";
 	$contenido .= "Correo: ".$_POST['email']."\r\n";
 	$contenido .= "Empresa: ".$_POST['empresa']."\r\n";
 	$contenido .= "Mensaje: ".$_POST['mensaje']."\r\n";
 	$contenido .= "el mensaje se escribio el ".$fecha;
-	$tucorreo="info@consultoriografico.com";
-	// Vamos a verificar si funciono el envio
 	if ($_POST['submit']) {
-		if (mail("acevallos@consultoriografico.com", $sujeto, utf8_decode($contenido), $header)) {
+		if (mail("info@consultoriografico.com", $sujeto, utf8_decode($contenido), $header)) {
 			echo 
 			"<script language='javascript'>
 				alert('Gracias por tu mensaje, pronto nos pondremos en contacto contigo.');
