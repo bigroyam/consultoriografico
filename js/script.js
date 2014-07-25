9db58d6e44b491dde4b75eb52f06f1d4ebdf1e8f
acevallos <andrescevallos@MacBook-Pro-de-Andres.local>
2014-07-25 09:22:02 -0500

    banner tipo slider

--- js/script.js
+++ js/script.js
@@ -1,50 +1,5 @@
 $(function(){
-	$('#btnSig').on('click', avanzar);
-	$('#btnAnt').on('click', retroceder);
 
-	setInterval(avanzar,6000);
+	$('#slider').cycle({ fx:'curtainY', next: '#btnSig', prev: '#btnAnt'});
 
-	function avanzar(){
-		var size = $('#slider').find('.elementos').size();
-		$('#slider').find('.elementos').each(function(index,value){
-			if($(value).hasClass('visible')) {
-				$(value).fadeOut();
-				$(value).removeClass('visible');
-
-				if(index+1<size) {
-					$($('#slider').find('.elementos').get(index+1)).fadeIn()
-					$($('#slider').find('.elementos').get(index+1)).addClass('visible');
-					return false;
-				}
-				else {
-					$($('#slider').find('.elementos').get(0)).fadeIn()
-					$($('#slider').find('.elementos').get(0)).addClass('visible');
-					return false;
-				}
-			}
-		});
-	};
-
-	function retroceder(){
-		var size = $('#slider').find('.elementos').size();
-		$('#slider').find('.elementos').each(function(index,value){
-			if($(value).hasClass('visible')) {
-				$(value).fadeOut();
-				$(value).removeClass('visible');
-
-				if(index==0) {
-					$($('#slider').find('.elementos').get(size-1)).fadeIn()
-					$($('#slider').find('.elementos').get(size-1)).addClass('visible');
-					return false;
-				}
-				else {
-					$($('#slider').find('.elementos').get(index-1)).fadeIn()
-					$($('#slider').find('.elementos').get(index-1)).addClass('visible');
-					return false;
-				}
-			}
-		});
-	};
-
-
-}); 
+});
